346283015a73d87f5118e6eac1195cbda2cd3274
Abhinav Srivastava <abhinav.srivastava5@epsilon.com>
2025-12-08 11:58:03 +0530

    Add HashMap and optimized array solutions for Valid Anagram
    - Add HashMap import for new solution implementation
    - Add isAnagramUsingHashMap method with O(n) time complexity
    - Add isAnagramUsingArray method with optimized O(n) solution
    - Update author tags and performance metrics for existing methods
    - Update runtime and memory statistics for isAnagramUsingTwoArrays and isAnagramUsingCharArray

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,14 +1,16 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
 
     /**
+     * @author: @ABHIYANSHU
      * Time Complexity: O(26) + O(26) + O(n) + O(n) + O(26) = O(n)
      * Space Complexity: O(26) + O(26) = O(1)
-     * Runtime: 2 ms, faster than 98.99% of Java online submissions for Valid Anagram.
-     * Memory Usage: 42.5 MB, less than 91.80% of Java online submissions for Valid Anagram.
+     * Runtime: 3 ms, faster than 90.18% of Java online submissions for Valid Anagram.
+     * Memory Usage: 44.24 MB, less than 70.29% of Java online submissions for Valid Anagram.
      */
     public boolean isAnagramUsingTwoArrays(String s, String t) {
         if (s.length() != t.length())
@@ -34,10 +36,11 @@ public class ValidAnagram {
     }
 
     /**
+     * @author: @ABHIYANSHU
      * Time Complexity: O(n) + O(n) + O(n log n) + O(n log n) + O(n) = O(n log n)
      * Space Complexity: O(n) + O(n) = O(n)
-     * Runtime: 4 ms, faster than 84.94% of Java online submissions for Valid Anagram.
-     * Memory Usage: 45.5 MB, less than 29.99% of Java online submissions for Valid Anagram.
+     * Runtime: 3 ms, faster than 90.18% of Java online submissions for Valid Anagram.
+     * Memory Usage: 46.45 MB, less than 18.93% of Java online submissions for Valid Anagram.
      */
     public boolean isAnagramUsingCharArray(String s, String t) {
         if (s.length() != t.length())
@@ -55,5 +58,74 @@ public class ValidAnagram {
         }
         
         return true;
+    }
+
+    /**
+     * @author: @ABHIYANSHU
+     * Time Complexity: O(n) + O(n) + O(n) = O(n)
+     * Space Complexity: O(n) = O(n)
+     * Runtime: 21 ms, faster than 09.86% of Java online submissions for Valid Anagram.
+     * Memory Usage: 46.58 MB, less than 12.87% of Java online submissions for Valid Anagram.
+     */
+    public boolean isAnagramUsingHashMap(String s, String t) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        if (s.length() != t.length())
+            return false;
+
+        for (int i = 0; i < s.length(); i++) { // O(n)
+            int ascii = (char)(s.charAt(i));
+            if (map.containsKey(ascii)) {
+                map.put(ascii, map.get(ascii) + 1);
+            } else {
+                map.put(ascii, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) { // O(n)
+            int ascii = (char)(t.charAt(i));
+            if (map.containsKey(ascii)) {
+                int val = map.get(ascii) - 1;
+                if (val < 0)
+                    return false;
+                
+                map.put(ascii, map.get(ascii) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        for (int i : map.keySet()) { // O(n)
+            if (map.get(i) != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @author: @LeetCode
+     * Time Complexity: O(n) + O(n) = O(n)
+     * Space Complexity: O(256) = O(1)
+     * Runtime: 2 ms, faster than 98.86% of Java online submissions for Valid Anagram.
+     * Memory Usage: 44.18 MB, less than 74.86% of Java online submissions for Valid Anagram.
+     */
+    public boolean isAnagramUsingArray(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[256];
+
+       for(char c:s.toCharArray()){ // O(n)
+            count[c]++;
+       }
+       for(char c:t.toCharArray()){ // O(n)
+            count[c]--;
+       
+       if(count[c] < 0)
+            return false;
+       } 
+
+       return true;
     }
 }
